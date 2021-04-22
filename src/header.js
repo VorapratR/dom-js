29d6d973c8001f2da12f951d70b4658a03834ae7
Voraprat Rattana <voraprat.r@dosetech.co>
2021-04-22 10:53:10 +0700

    [Merge] cookie-consent-header and cookieconsent

--- src/header.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var h4 = document.createElement("h4");
-    h4.textContent = "something";
-    console.log(document.body);
-    document.body.appendChild(h4);
-    var h3 = document.createElement("h3");
-    h3.textContent = "something3";
-    document.body.appendChild(h3)
-}
