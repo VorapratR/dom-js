3a737e3827464b4ee0ac51ec1aaf4448e162ad61
Voraprat Rattana <voraprat.r@dosetech.co>
2021-04-22 09:27:38 +0700

    [Check] init data without init.js

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -1,3 +1,33 @@
+var data = {
+    messageInCookieBarText: "บริษัท Test จำกัดใช้คุกกี้เพื่อให้ท่านได้รับประสบการณ์การใช้งานที่ดียิ่งขึ้น",
+    privacyPolicyText: "อ่านนโยบายการคุ้มครองข้อมูลส่วนบุคคล",
+    andText: "และ",
+    cookiePolicyText: "นโยบายคุกกี้",
+    editCookieText: "ปรับแต่งคุกกี้",
+    submitCookieText: "ยอมรับทั้งหมด",
+    modalModifyCookieTitleText: "การตั้งค่าคุกกี้",
+    modalBodyMessageText: "ข้าพเจ้ายินยอมให้ทางบริษัทใช้ Cookies ประเภท Analytics Cookies " +
+        "ที่ทำหน้าที่ประเมินประสิทธิ ภาพในการทำงานแต่ละส่วนของเว็บไซต์ และการปรับปรุง ซึ่งอาจเป็นส่วนที่บริษัทดำเนินการเอง " +
+        " หรือ ว่าจ้างบุคคลภายนอกดำเนินการดังกล่าวให้ โดยดำเนินการภายใต้ เงื่อนไขนโยบายข้อมูลส่วนบุคคลของ บริษัท",
+    saveAcceptText: "บันทึกการตั้งค่า",
+    allAcceptText: "ยอมรับทั้งหมด",
+
+    privacyPolicyURL: "https://nawaplastic.com/th/privacy-policy.php",
+    cookiePolicyURL: "https://nawaplastic.com/th/privacy-policy.php",
+
+    showMoreText: "แสดงมากขึ้น",
+    showLessText: "แสดงน้อยลง",
+    showMoreOperator: 'มาก',
+
+    necessaryTitleText: "Strictly Necessary Cookies",
+    necessaryStatusText: "เปิดใช้งานตลอดเวลา",
+    necessaryDescriptionText: "Strictly Necessary cookies  Necessary cookies เพื่อช่วยให้การทำงานหลักของเว็บไซต์ใช้งานได้",
+    necessaryMoreMessageText: "รวมถึงการเข้าถึงพื้นที่ที่ปลอดภัยต่าง ๆ ของเว็บไซต์ หากไม่มีคุกกี้นี้เว็บไซต์จะไม่สามารถทำงานได้อย่างเหมาะสม และจะใช้งานได้โดยการตั้งค่าเริ่มต้น โดยไม่สามารถปิดการใช้งานได้",
+
+    analyticsTitleText: "Analytics Cookies",
+    analyticsDescriptionText: "Analytics cookies จะช่วยให้เว็บไซต์เข้าใจรูปแบบการใช้งานของผู้เข้าชม",
+    analyticsMoreMessageText: "และจะช่วยปรับปรุงประสบการณ์การใช้งาน โดยการเก็บรวบรวมข้อมูลและรายงานผลการใช้งานของผู้ใช้งาน"
+}
 window.onload = function () {
     writeHTML();
     checkedConsentInApp(true);
@@ -8,12 +38,12 @@ window.onload = function () {
 }
 
 function writeHTML() {
-    let data = CONSENTCOOKIE.getArgs();
+    // let data = CONSENTCOOKIE.getArgs();
     let ccmain = document.getElementById('cookieconsent');
     // cookie bar start
     let cookieBar = document.createElement('div');
     cookieBar.id = "cookieBar";
-    cookieBar.className = "container-fluid px-3 py-3 px-lg-4 py-lg-3  my-2 d-flex justify-content-center fixed-bottom ";
+    cookieBar.className = "container-fluid fluid-r-l px-3 py-3 px-lg-4 py-lg-3  my-2 d-flex justify-content-center fixed-bottom ";
 
     let cookieBarRow = document.createElement('div');
     cookieBarRow.className = "row";
@@ -280,7 +310,7 @@ function checkedConsentInApp(fromWindowLoad) {
 
     if (consentCookiePreference) {
         consentCookiePreference = JSON.parse(consentCookiePreference);
-        let data = CONSENTCOOKIE.getArgs();
+        // let data = CONSENTCOOKIE.getArgs();
         if (consentCookiePreference.analytics) {
             unblockAnalytics();
         }
@@ -303,7 +333,7 @@ function unblockAnalytics() {
 }
 
 function setDefaultInnerHTML() {
-    let data = CONSENTCOOKIE.getArgs();
+    // let data = CONSENTCOOKIE.getArgs();
 
     document.getElementById("messageInCookieBar").innerHTML = `
     ${data.messageInCookieBarText} 
@@ -341,7 +371,7 @@ function setDefaultInnerHTML() {
 }
 
 function collapseDescription() {
-    let data = CONSENTCOOKIE.getArgs();
+    // let data = CONSENTCOOKIE.getArgs();
 
     $('#necessaryMoreId').click(function () {
         $(this).text(function (i, old) {
@@ -356,7 +386,7 @@ function collapseDescription() {
 }
 
 function setDefaultHref() {
-    let data = CONSENTCOOKIE.getArgs();
+    // let data = CONSENTCOOKIE.getArgs();
     document.getElementById("privacyPolicy").href = data.privacyPolicyURL;
     document.getElementById("cookiePolicy").href = data.cookiePolicyURL;
 }
