7521eb803df72382c669a438fc02be7a9b07557c
Voraprat Rattana <voraprat.r@dosetech.co>
2021-04-22 10:01:59 +0700

    [Add] YETT_BLACKLIST in cookie consent

--- src/cookie-consent-header.js
+++ src/cookie-consent-header.js
@@ -28,8 +28,15 @@ var data = {
     analyticsDescriptionText: "Analytics cookies จะช่วยให้เว็บไซต์เข้าใจรูปแบบการใช้งานของผู้เข้าชม",
     analyticsMoreMessageText: "และจะช่วยปรับปรุงประสบการณ์การใช้งาน โดยการเก็บรวบรวมข้อมูลและรายงานผลการใช้งานของผู้ใช้งาน"
 }
-
-window.onload = function () {
+window.YETT_BLACKLIST = [
+        /www\.google-analytics\.com/,
+        /www\.googletagmanager\.com/
+    ]
+    // // Or a whitelist
+    // window.YETT_WHITELIST = [
+    //     /my-whitelisted-domain/,
+    // ]
+window.onload = function() {
     writeHTML();
     checkedConsentInApp(true);
     setDefaultInnerHTML();
@@ -166,7 +173,7 @@ function writeHTML() {
 
     let modalBody = document.createElement('div')
     modalBody.className = "px-3"
-    // Strictly Necessary Cookies
+        // Strictly Necessary Cookies
     let modalNecessaryCard = document.createElement('div');
     modalNecessaryCard.className = "card-black";
     let modalNecessaryCardBody = document.createElement('div');
@@ -272,11 +279,11 @@ function saveAcceptClick() {
     let newCookie = " consent-cookie-preference" + '=' + JSON.stringify(dataCookie) + ";"
     if (readCookie('consent-cookie')) {
         document.cookie = newCookie
-        // console.log(document.cookie);
+            // console.log(document.cookie);
     } else {
         document.cookie = "consent-cookie" + "=" + "allows" + ";"
         document.cookie = newCookie
-        // console.log(document.cookie);
+            // console.log(document.cookie);
     }
     $('#modalModifyCookie').modal('hide');
     checkedConsentInApp(false);
@@ -368,13 +375,13 @@ function setDefaultInnerHTML() {
 }
 
 function collapseDescription() {
-    $('#necessaryMoreId').click(function () {
-        $(this).text(function (i, old) {
+    $('#necessaryMoreId').click(function() {
+        $(this).text(function(i, old) {
             return old.includes(data.showMoreOperator) ? data.showLessText : data.showMoreText;
         });
     });
-    $('#analyticsMoreId').click(function () {
-        $(this).text(function (i, old) {
+    $('#analyticsMoreId').click(function() {
+        $(this).text(function(i, old) {
             return old.includes(data.showMoreOperator) ? data.showLessText : data.showMoreText;
         });
     });
